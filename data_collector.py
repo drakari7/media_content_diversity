ea6beb91417c814101a6426e356a1c829a64d74e
drakari7 <shreyash.ravi@gmail.com>
2021-07-27 05:18:44 +0530

    chore: update all hsv files

--- data_collector.py
+++ data_collector.py
@@ -66,8 +66,7 @@ def get_urls(driver, channel_name, time_range):
     try:
         wait_till_visible_xpath(driver, posting_time_xpath)
     except:
-        logging.exception(f"Could not load {channel_name}'s \
-                            youtube page properly")
+        logging.exception(f"Could not load {channel_name}'s youtube page properly")
     posting_time = driver.find_elements_by_xpath(posting_time_xpath)
     logging.info(f"Collecting urls for channel {channel_name}")
 
@@ -78,15 +77,17 @@ def get_urls(driver, channel_name, time_range):
         try:
             posting_time = driver.find_elements_by_xpath(posting_time_xpath)
         except:
-            logging.exception(f"Unknown error in \
-                              URL collection - {channel_name}")
+            logging.exception(f"Unknown error in URL collection - {channel_name}")
             continue
 
         logging.info(f"Collected {len(posting_time)} URLs - {channel_name}")
         time.sleep(0.15)     # Give time to load
 
     # Collecting all relevant video urls
+    t1 = time.perf_counter()
     titles = driver.find_elements_by_id("video-title")
+    t2 = time.perf_counter()
+    logging.info(f"time taken to get elements {t2-t1} - {channel_name}")
     urls = [title.get_attribute("href") for title in titles]
     titles = [i.text for i in titles]
     return (urls, titles)
@@ -102,8 +103,7 @@ def process_urls(driver, channel_name, file_name, urls) -> None:
             try:
                 line = _process_url(driver, url)
             except:
-                logging.exception(f"Corrupted URL : {url}, \
-                                    video no. {idx}, {channel_name}")
+                logging.exception(f"Corrupted URL : {url}, video no. {idx}, {channel_name}")
                 skipped_videos_count += 1
                 continue
 
@@ -121,23 +121,23 @@ def _process_url(driver, url):
     # Wait for title to load, if can't then throw error
     title_xpath = '//*[@id="container"]/h1/yt-formatted-string'
     wait_till_visible_xpath(driver, title_xpath)
-    title = driver.find_element_by_xpath(title_xpath).text
+    title = driver.find_element_by_xpath(title_xpath).get_attribute("innerText")
 
     views_xpath = '//*[@id="count"]/ytd-video-view-count-renderer/span[1]'
-    views = driver.find_element_by_xpath(views_xpath).text
+    views = driver.find_element_by_xpath(views_xpath).get_attribute("innerText")
 
     date_xpath = '//*[@id="info-strings"]/yt-formatted-string'
-    date = driver.find_element_by_xpath(date_xpath).text
+    date = driver.find_element_by_xpath(date_xpath).get_attribute("innerText")
 
     # duration_xpath = "//span[@class='ytp-time-duration']"
-    # duration = driver.find_element_by_xpath(duration_xpath).text
+    # duration = driver.find_element_by_xpath(duration_xpath).get_attribute("innerText")
 
     keywords = driver.find_element_by_name("keywords")
     keywords = keywords.get_attribute("content")
     keywords = ' '.join(keywords.splitlines())
 
     description_xpath = '//*[@id="description"]/yt-formatted-string'
-    description = driver.find_element_by_xpath(description_xpath).text
+    description = driver.find_element_by_xpath(description_xpath).get_attribute("innerText")
     description = ' '.join(description.splitlines())
 
     return '\\#\\'.join([title, views, date, keywords, description])
@@ -153,7 +153,7 @@ def scrape_channel_data(channel_link):
         driver.get(channel_link)
         try:
             video_urls, _ = get_urls(driver, channel_name, 
-                                     time_range="3 days ago")
+                                     time_range="3 months ago")
         except:
             logging.exception(f"Getting URL function failed! - {channel_name}")
             raise
@@ -174,7 +174,7 @@ def scrape_channel_data(channel_link):
 def main():
     g_start = time.perf_counter()
 
-    channels = tl.get_channel_links()[1:2]
+    channels = tl.get_temp_links()
     # channels = tl.get_testing_channel()        # Uncomment for testing
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
